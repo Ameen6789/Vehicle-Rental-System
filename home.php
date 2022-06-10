e51d4cc954ef3bfca5a13733fab47abc3c02f015
Mufeeda-hash <70136843+Mufeeda-hash@users.noreply.github.com>
2022-06-10 20:14:06 +0530

    home.php

---
+++ home.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style type="text/css">
+         .topnav {
+    background-color: #333;
+    overflow: hidden;
+  }
+  
+  /* Style the links inside the navigation bar */
+  .topnav a {
+    float: right;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
+  }
+  
+  /* Change the color of links on hover */
+  .topnav a:hover {
+    background-color: #ddd;
+    color: black;
+  }
+  
+  /* Add a color to the active/current link */
+  .topnav a.active {
+    background-color: #04AA6D;
+    color: white;
+  }
+    </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+   
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    ?>
+<nav id="id1" class="topnav">
+     <a href="login.php">LOGIN</a>
+     <a href="signup.php">SIGNUP</a>
+     <a href="about.html">ABOUT US</a>
+     <a href="admin.php">ADMIN</a>
+     
+</nav>
+
+
+</body>
+</html>
