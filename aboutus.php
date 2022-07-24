a02fe9c2c881e4754d1c5c86a5641ec57750ab23
Mufeeda Muhammed <70136843+Mufeeda-07@users.noreply.github.com>
2022-07-24 16:44:25 +0530

    aboutus.php

---
+++ aboutus.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+    font-family: Helvetica;
+    -webkit-font-smoothing: antialiased;
+    background: rgba( 71, 147, 227, 1);
+}
+h1,h2{
+    text-align: center;
+    font-size: 30px;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    color: white;
+    padding: 30px 0;
+}
+.topnav {
+    background-color: #333;
+    overflow: hidden;
+  }
+  
+   /* Style the links inside the navigation bar  */
+  .topnav a {
+    float: right;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
+  }
+  */
+  
+   /* Change the color of links on hover  */
+  .topnav a:hover {
+    background-color: #ddd;
+    color: black;
+  }
+  
+   /* Add a color to the active/current link  */
+  .topnav a.active {
+    background-color: #04AA6D;
+    color: white;
+  }
+
+        </style>
+</head>
+<body>
+<nav class="topnav">
+
+<a href="home.php">HOME</a>
+</nav>
+    <center><h1>VEHICLE RENTAL SYSTEM</h1></center>
+    <div>
+
+       <h3> <p>This is one of the most trusted vehicle rental service.The rent-a-vehicle service provider offers an<br> outstanding model and wide varity of
+    vehicle options at the most competitive rates.We provide bikes and cars that can meet your every need.<br>
+    Also we allow you to enjoy flexibility with respect to start and endpoints.
+    You can book the vehicle on an daily weekly monthly basis withou any<br> security deposit. We provide door step delivery and we believe in 
+    complete transparancy and adhere strictly business ethics.This means that you<br> no need to worry about the hidden chargeswhen renting vehicle from us.
+</p></h3> </div>
+<img src="home.jpg" alt="" srcset="" class="abc">
+<br>
+<h2>LOCATION : POVVAL CONTACT:98765443210</h2>
+
+</body>
+</html>
